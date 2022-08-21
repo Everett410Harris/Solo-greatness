5ec4b43bb19018b83b4c701af8573d705ba668f3
Everett Harris <everettharr979@gmail.com>
2022-08-20 20:12:41 -0400

    form layout

--- SoloApp/Form1.Designer.cs
+++ SoloApp/Form1.Designer.cs
@@ -28,12 +28,389 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.licensePlateLabel = new System.Windows.Forms.Label();
+            this.makeLabel = new System.Windows.Forms.Label();
+            this.modelLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.engineTypeLabel = new System.Windows.Forms.Label();
+            this.colorLabel = new System.Windows.Forms.Label();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.mileageLabel = new System.Windows.Forms.Label();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.displayBtn = new System.Windows.Forms.Button();
+            this.addNewBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1339, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.searchBtn);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Location = new System.Drawing.Point(22, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1296, 74);
+            this.panel2.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 405);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1339, 287);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Location = new System.Drawing.Point(12, 716);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1339, 97);
+            this.panel3.TabIndex = 11;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.comboBox5);
+            this.panel4.Controls.Add(this.textBox4);
+            this.panel4.Controls.Add(this.comboBox4);
+            this.panel4.Controls.Add(this.textBox3);
+            this.panel4.Controls.Add(this.comboBox3);
+            this.panel4.Controls.Add(this.comboBox2);
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Controls.Add(this.comboBox1);
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Controls.Add(this.licensePlateLabel);
+            this.panel4.Controls.Add(this.makeLabel);
+            this.panel4.Controls.Add(this.typeLabel);
+            this.panel4.Controls.Add(this.mileageLabel);
+            this.panel4.Controls.Add(this.modelLabel);
+            this.panel4.Controls.Add(this.yearLabel);
+            this.panel4.Controls.Add(this.colorLabel);
+            this.panel4.Controls.Add(this.engineTypeLabel);
+            this.panel4.Location = new System.Drawing.Point(26, 151);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1325, 248);
+            this.panel4.TabIndex = 12;
+            // 
+            // licensePlateLabel
+            // 
+            this.licensePlateLabel.AutoSize = true;
+            this.licensePlateLabel.Location = new System.Drawing.Point(7, 14);
+            this.licensePlateLabel.Name = "licensePlateLabel";
+            this.licensePlateLabel.Size = new System.Drawing.Size(232, 38);
+            this.licensePlateLabel.TabIndex = 2;
+            this.licensePlateLabel.Text = "License Plate";
+            // 
+            // makeLabel
+            // 
+            this.makeLabel.AutoSize = true;
+            this.makeLabel.Location = new System.Drawing.Point(7, 103);
+            this.makeLabel.Name = "makeLabel";
+            this.makeLabel.Size = new System.Drawing.Size(107, 38);
+            this.makeLabel.TabIndex = 3;
+            this.makeLabel.Text = "Make";
+            // 
+            // modelLabel
+            // 
+            this.modelLabel.AutoSize = true;
+            this.modelLabel.Location = new System.Drawing.Point(7, 192);
+            this.modelLabel.Name = "modelLabel";
+            this.modelLabel.Size = new System.Drawing.Size(120, 38);
+            this.modelLabel.TabIndex = 4;
+            this.modelLabel.Text = "Model";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 44);
+            this.textBox1.TabIndex = 11;
+            // 
+            // engineTypeLabel
+            // 
+            this.engineTypeLabel.AutoSize = true;
+            this.engineTypeLabel.Location = new System.Drawing.Point(472, 108);
+            this.engineTypeLabel.Name = "engineTypeLabel";
+            this.engineTypeLabel.Size = new System.Drawing.Size(218, 38);
+            this.engineTypeLabel.TabIndex = 6;
+            this.engineTypeLabel.Text = "Engine Type";
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.AutoSize = true;
+            this.colorLabel.Location = new System.Drawing.Point(472, 197);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(108, 38);
+            this.colorLabel.TabIndex = 7;
+            this.colorLabel.Text = "Color";
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(472, 14);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(94, 38);
+            this.yearLabel.TabIndex = 5;
+            this.yearLabel.Text = "Year";
+            // 
+            // mileageLabel
+            // 
+            this.mileageLabel.AutoSize = true;
+            this.mileageLabel.Location = new System.Drawing.Point(952, 14);
+            this.mileageLabel.Name = "mileageLabel";
+            this.mileageLabel.Size = new System.Drawing.Size(145, 38);
+            this.mileageLabel.TabIndex = 8;
+            this.mileageLabel.Text = "Mileage";
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(982, 103);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(96, 38);
+            this.typeLabel.TabIndex = 9;
+            this.typeLabel.Text = "Type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(957, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 38);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Booked";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(245, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(219, 46);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(219, 44);
+            this.textBox2.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(696, 14);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(219, 46);
+            this.comboBox2.TabIndex = 14;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(696, 100);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(219, 46);
+            this.comboBox3.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(696, 189);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(219, 44);
+            this.textBox3.TabIndex = 16;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(1103, 100);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(219, 46);
+            this.comboBox4.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1103, 11);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(219, 44);
+            this.textBox4.TabIndex = 18;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(1103, 184);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(219, 46);
+            this.comboBox5.TabIndex = 19;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Controls.Add(this.addNewBtn);
+            this.panel5.Controls.Add(this.displayBtn);
+            this.panel5.Controls.Add(this.deleteBtn);
+            this.panel5.Controls.Add(this.resetBtn);
+            this.panel5.Controls.Add(this.exitBtn);
+            this.panel5.Location = new System.Drawing.Point(22, 10);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1296, 74);
+            this.panel5.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(387, 17);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(342, 44);
+            this.textBox5.TabIndex = 20;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(793, 17);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(179, 44);
+            this.searchBtn.TabIndex = 21;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(1077, 13);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(202, 44);
+            this.exitBtn.TabIndex = 22;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(822, 13);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(216, 44);
+            this.resetBtn.TabIndex = 23;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(546, 13);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(237, 44);
+            this.deleteBtn.TabIndex = 24;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // displayBtn
+            // 
+            this.displayBtn.Location = new System.Drawing.Point(270, 13);
+            this.displayBtn.Name = "displayBtn";
+            this.displayBtn.Size = new System.Drawing.Size(237, 44);
+            this.displayBtn.TabIndex = 25;
+            this.displayBtn.Text = "Display";
+            this.displayBtn.UseVisualStyleBackColor = true;
+            // 
+            // addNewBtn
+            // 
+            this.addNewBtn.Location = new System.Drawing.Point(20, 13);
+            this.addNewBtn.Name = "addNewBtn";
+            this.addNewBtn.Size = new System.Drawing.Size(211, 44);
+            this.addNewBtn.TabIndex = 26;
+            this.addNewBtn.Text = "Add New";
+            this.addNewBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 38F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(1363, 825);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button searchBtn;
+        private TextBox textBox5;
+        private DataGridView dataGridView1;
+        private Panel panel3;
+        private Panel panel5;
+        private Button addNewBtn;
+        private Button displayBtn;
+        private Button deleteBtn;
+        private Button resetBtn;
+        private Button exitBtn;
+        private Panel panel4;
+        private ComboBox comboBox5;
+        private TextBox textBox4;
+        private ComboBox comboBox4;
+        private TextBox textBox3;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Label label6;
+        private Label licensePlateLabel;
+        private Label makeLabel;
+        private Label typeLabel;
+        private Label mileageLabel;
+        private Label modelLabel;
+        private Label yearLabel;
+        private Label colorLabel;
+        private Label engineTypeLabel;
     }
 }
